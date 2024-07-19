bfca6b3f0ae1bcc36ed06049d857bf823c75c582
AakashHubGit <aakash.pandey@somaiya.edu>
2024-07-20 05:15:44 +0530

    Auth Endpoint

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const connectToMongo = require("./db/connection");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+connectToMongo();
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/user.routes"));
+// app.use('/api/turf', require('./routes/turf'))
+// app.use('/api/booking', require('./routes/booking'))
+// app.use('/api/products', require('./routes/products'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
